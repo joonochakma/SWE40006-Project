88f17b2c664eb47f8b940e8443a3a1dc87a5a36b
adoniaspedro <104463681@student.swin.edu.au>
2025-10-16 16:11:12 +1100

    Initial Media Player Frontend

--- Frontend/my-frontend/src/App.jsx
+++ Frontend/my-frontend/src/App.jsx
@@ -1,10 +1,29 @@
-import { useState } from 'react'
+import { useState, useRef  } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fileInputRef = useRef(null) // Reference to the hidden file input
+
+  const UploadClick = () => {
+    // Trigger the hidden file input click
+    fileInputRef.current?.click()
+  }
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0]
+    if (file) {
+      // Check if it's an audio file
+      if (file.type.startsWith('audio/')) {
+        console.log('Selected Audio File:', file.name)
+      } else {
+        alert('Please Select an Audio File')
+        // Reset the input
+        event.target.value = ''
+      }
+    }
+  }
 
   return (
     <>
@@ -15,7 +34,17 @@ function App() {
         {}
       </div>
 
-      <button id="upload-button">Upload Audio File</button>
+       <input
+          type="file"
+          ref={fileInputRef}
+          onChange={handleFileChange}
+          accept="audio/*" // Only allow audio files
+          style={{ display: 'none' }} // Hide the actual file input
+        />
+
+       <button id="upload-button" onClick={UploadClick}>
+          Upload Audio File
+      </button>
     </>
   )
 }
